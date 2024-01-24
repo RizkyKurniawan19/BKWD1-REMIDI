2972aa146bf927b38136c9bbd359280ff2ca9aa2
RizkyKurniawan19 <101341800+RizkyKurniawan19@users.noreply.github.com>
2024-01-25 00:15:12 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'Poli_BK';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+?>
